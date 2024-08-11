4229a9725ded954b21d65d1f444db14ea0f78380
G.Ashrith <156601328+G-Ashrith@users.noreply.github.com>
2024-08-11 22:57:18 +0530

    Update Task-2 StudentGradeCalculator.java

--- Task-2 StudentGradeCalculator.java
+++ Task-2 StudentGradeCalculator.java
@@ -1,26 +1,19 @@
 
-
 import java.util.Scanner;
-
 public class StudentGradeCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
         System.out.print("Enter number of subjects: ");
         int n = sc.nextInt();
-        
         int[] m = new int[n];
         int t = 0;
-        
         for (int i = 0; i < n; i++) {
             System.out.print("Enter marks for subject " + (i + 1) + ": ");
             m[i] = sc.nextInt();
             t += m[i];
         }
-        
         double avg = (double) t / n;
         String g;
-        
         if (avg >= 90) g = "A+";
         else if (avg >= 80) g = "A";
         else if (avg >= 70) g = "B";
